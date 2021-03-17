9d85644dba6ea14a89b17d77d82c3d360903ab63
Pedro Lima <pedrofelixcfn@gmail.com>
2021-03-17 20:12:39 -0300

    "prototipo de listagem concluido"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { DropdownModule } from 'primeng/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
 
 
 import { AppComponent } from './app.component';
 import { ListagemTarefasComponent } from './listagem-tarefas/listagem-tarefas.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CadastrarTarefaComponent } from './cadastrar-tarefa/cadastrar-tarefa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListagemTarefasComponent,
-    NavbarComponent
+    NavbarComponent,
+    CadastrarTarefaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DropdownModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    TableModule,
+    InputTextareaModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/listagem-tarefas/listagem-tarefas.component.ts
+++ src/app/listagem-tarefas/listagem-tarefas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Responsavel } from './Responsavel';
+
 
 @Component({
   selector: 'app-listagem-tarefas',
@@ -7,6 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListagemTarefasComponent  {
 
-  
+  responsaveis: Responsavel[];
+
+  select: Responsavel;
+
+  constructor() {
+      this.responsaveis = [
+          {name: 'Em andamento'},
+          {name: 'Concluido'}
+      ];
+
+      this.select={name:""}
+  }
+
+  tarefas: any = [
+
+    { numero: '1', titulo: 'Jogar', responsavel: 'Pedro'},
+
+    { numero: '2', titulo: 'Lavar louça', responsavel: 'Pedro'},
+
+    { numero: '3', titulo: 'Alimentar tartaruga', responsavel: 'Pedro'},
+
+    { numero: '4', titulo: 'Jogar', responsavel: 'Ester'},
+
+    { numero: '5', titulo: 'Tirar lixo', responsavel: 'Pedro',},
+
+    { numero: '6', titulo: 'Almoçar', responsavel: 'Todos'},
+
+    { numero: '7', titulo: 'Programar', responsavel: 'pedro'},
+
+    { numero: '8', titulo: 'Fazer culto', responsavel: 'Vanessa'},
+
+    { numero: '9', titulo: 'jogar', responsavel: 'Vanessa'}
+  ]
+ 
 
 }
